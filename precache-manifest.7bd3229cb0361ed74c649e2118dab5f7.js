7721c136c405158fcacedc7756d95d6d3b89a14d
Ronaiza Cardoso <ronaizacard@gmail.com>
2020-05-06 16:47:47 +0100

    Updates

---
+++ precache-manifest.7bd3229cb0361ed74c649e2118dab5f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f7c21e9b3dd36944e2ab1ad128a84a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96e226345041ad9b874f",
+    "url": "/static/css/main.70334752.chunk.css"
+  },
+  {
+    "revision": "8cb1833e606d4257560a",
+    "url": "/static/js/2.47220973.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.47220973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96e226345041ad9b874f",
+    "url": "/static/js/main.75fe8638.chunk.js"
+  },
+  {
+    "revision": "02532af204b0d2121d8e",
+    "url": "/static/js/runtime-main.3f059d60.js"
+  }
+]);
